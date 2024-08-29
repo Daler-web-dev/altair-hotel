15cd888aef881e7c30eec7a2bec0b6d23b2526e0
Daler <dsarifkulo021@gmail.com>
2024-08-29 17:43:25 +0500

    trying to solve problem

--- middleware.js
+++ middleware.js
@@ -1,44 +1,58 @@
-import { NextResponse } from "next/server";
+import {
+    NextResponse
+} from "next/server";
 
-import { i18n } from "@/i18n.config";
+import {
+    i18n
+} from "@/i18n.config";
 
-import { match as matchLocale } from "@formatjs/intl-localematcher";
+import {
+    match as matchLocale
+} from "@formatjs/intl-localematcher";
 import Negotiator from "negotiator";
 
 function getLocale(request) {
-    const negotiatorHeaders = {};
-    request.headers.forEach((value, key) => (negotiatorHeaders[key] = value));
+    try {
+        const negotiatorHeaders = {};
+        request.headers.forEach((value, key) => (negotiatorHeaders[key] = value));
 
-    // @ts-ignore locales are readonly
-    const locales = i18n.locales;
-    const languages = new Negotiator({
-        headers: negotiatorHeaders,
-    }).languages();
+        const locales = i18n.locales;
+        const languages = new Negotiator({
+            headers: negotiatorHeaders,
+        }).languages();
 
-    const locale = matchLocale(languages, locales, i18n.defaultLocale);
-    return locale;
+        const locale = matchLocale(languages, locales, i18n.defaultLocale);
+        return locale;
+    } catch (error) {
+        console.error('Error getting locale:', error);
+        return i18n.defaultLocale; // Возвращаем локаль по умолчанию в случае ошибки
+    }
 }
 
 export function middleware(request) {
-    const pathname = request.nextUrl.pathname;
+    try {
+        const pathname = request.nextUrl.pathname;
 
-    const pathnameIsMissingLocale = i18n.locales.every(
-        (locale) =>
+        const pathnameIsMissingLocale = i18n.locales.every(
+            (locale) =>
             !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
-    );
-
-    // Redirect if there is no locale
-
-    if (pathnameIsMissingLocale && pathname !== "/robots.txt" && pathname !== "/sitemap.xml") {
-        const locale = getLocale(request);
-        return NextResponse.redirect(
-            new URL(
-                `/${locale}${pathname.startsWith("/") ? "" : "/"}${pathname}`,
-                request.url
-            )
         );
+
+        if (pathnameIsMissingLocale && pathname !== "/robots.txt" && pathname !== "/sitemap.xml") {
+            const locale = getLocale(request);
+            return NextResponse.redirect(
+                new URL(
+                    `/${locale}${pathname.startsWith("/") ? "" : "/"}${pathname}`,
+                    request.url
+                )
+            );
+        }
+    } catch (error) {
+        console.error('Middleware error:', error);
+        return NextResponse.next(); // Возвращаем следующий ответ, чтобы сервер не падал
     }
 }
+
 
 export const config = {
     matcher: [
